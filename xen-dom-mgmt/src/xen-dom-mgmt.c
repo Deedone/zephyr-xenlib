94365482231821d0e06f1f7b7843ff191fdaad60
Mykyta Poturai <mykyta_poturai@epam.com>
2024-05-07 09:05:44 +0300

    xen_cmds: Allow to use domain name instead of domid in commands
    Add the ability to use domain name instead of domid in the xu shell
    commands. If the passed identifier is all numbers, then it is
    considered as domid, otherwise, it is considered as domain name.
    Updated commands description to reflect this change.
    Affected commands: destroy, pause, unpause, console.
    
    With this patch, the following commands become equivalent:
    xu destroy 1
    xu destroy DomD
    Assuming that domain 1 has name "DomD".
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Reviewed-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

--- xen-dom-mgmt/src/xen-dom-mgmt.c
+++ xen-dom-mgmt/src/xen-dom-mgmt.c
@@ -13,6 +13,7 @@
 #include <zephyr/xen/public/hvm/hvm_op.h>
 #include <zephyr/xen/public/hvm/params.h>
 #include <zephyr/xen/public/domctl.h>
+#include <zephyr/xen/dom0/sysctl.h>
 #include <zephyr/xen/public/xen.h>
 
 #include <zephyr/xen/public/io/console.h>
@@ -648,6 +649,32 @@ int get_domain_name(unsigned short domain_id, char *name, int len)
 #else
 	return -EINVAL;
 #endif
+}
+
+uint32_t find_domain_by_name(char *arg)
+{
+	char domname[CONTAINER_NAME_SIZE];
+	struct xen_domctl_getdomaininfo infos[CONFIG_DOM_MAX];
+	uint32_t domid = 0;
+	int i, ret;
+
+	ret = xen_sysctl_getdomaininfo(infos, 0, CONFIG_DOM_MAX);
+	if (ret < 0) {
+		goto out;
+	}
+
+	for (i = 0; i < ret; i++) {
+		if (!get_domain_name(infos[i].domain, domname,
+					    CONTAINER_NAME_SIZE)) {
+			if (strcmp(domname, arg) == 0) {
+				domid = infos[i].domain;
+				break;
+			}
+		}
+	}
+
+out:
+	return domid;
 }
 
 __weak int domain_get_user_cfg_count(void)
